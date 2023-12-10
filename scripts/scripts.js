7aac409203926346e52f976e597272f2ce042bac
SamaraFellaDina <sammybensink@gmail.com>
2023-12-10 21:07:11 +0100

    niet meer huilen :D

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,11 @@
 // Selecteer een element
-var AddListButton = document.querySelector("add-button-saved-houses");
-var PopUpAddHouse = document.querySelector("pop-up-hidden");
+var AddListButton = document.querySelector(".add-button-saved-houses");
+var PopUpAddHouse = document.querySelector(".pop-up-hidden");
 
 
 // Luisteren naar het gebruikers event
 AddListButton.addEventListener("click", function() {
-    PopUpAddHouse.classList.add("pop-up-show");
+
+    // 3 - Koppelen van een actie classlist en CSS
+    PopUpAddHouse.classList.toggle("pop-up-show");
   });
